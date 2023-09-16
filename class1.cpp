e5afb017dcbffe94342ed9abbdf00d831610d06d
Pulkit Tyagi <pulkitniraj@gmail.com>
2023-09-17 02:48:35 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class student{
+	public:
+		 string name;
+		 string  phone;
+};
+int main(){
+	student s;
+	s.name="pulkit";
+	s.phone="9650819336";
+	cout<<s.name<<endl;
+	cout<<s.phone<<endl;
+}
---
+++ struct1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+struct student{
+	string name;
+};
+int main(){
+	struct  student s;
+	s.name="pulkit";
+	cout<<s.name<<endl;
+	
+}
